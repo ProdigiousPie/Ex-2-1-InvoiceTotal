3559116941a033ace77785f8ad67d12d52fb4cf5
Brendan Donnal <ye4559gl@go.minnstate.edu>
2020-08-30 17:56:27 -0500

    Completed Exercise 1b

--- InvoiceTotal/frmInvoiceTotal.Designer.cs
+++ InvoiceTotal/frmInvoiceTotal.Designer.cs
@@ -59,7 +59,7 @@ namespace InvoiceTotal
             this.label2.Location = new System.Drawing.Point(11, 38);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 13);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 2;
             this.label2.Text = "Discount Percent:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -69,7 +69,7 @@ namespace InvoiceTotal
             this.label3.Location = new System.Drawing.Point(12, 65);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(91, 13);
-            this.label3.TabIndex = 2;
+            this.label3.TabIndex = 6;
             this.label3.Text = "Discount Amount:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -79,7 +79,7 @@ namespace InvoiceTotal
             this.label4.Location = new System.Drawing.Point(12, 91);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 13);
-            this.label4.TabIndex = 3;
+            this.label4.TabIndex = 8;
             this.label4.Text = "Total:";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -122,8 +122,9 @@ namespace InvoiceTotal
             this.btnCalculate.Location = new System.Drawing.Point(44, 122);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(75, 23);
-            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.TabIndex = 4;
             this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // btnExit
             // 
@@ -131,17 +132,18 @@ namespace InvoiceTotal
             this.btnExit.Location = new System.Drawing.Point(131, 122);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
-            this.btnExit.TabIndex = 3;
+            this.btnExit.TabIndex = 5;
             this.btnExit.Text = "E&xit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // txtDiscountPercent
             // 
             this.txtDiscountPercent.Location = new System.Drawing.Point(106, 35);
             this.txtDiscountPercent.Name = "txtDiscountPercent";
-            this.txtDiscountPercent.ReadOnly = true;
             this.txtDiscountPercent.Size = new System.Drawing.Size(100, 20);
-            this.txtDiscountPercent.TabIndex = 10;
+            this.txtDiscountPercent.TabIndex = 3;
             this.txtDiscountPercent.TabStop = false;
+            this.txtDiscountPercent.Text = "5";
             // 
             // txtDiscountAmount
             // 
@@ -149,7 +151,7 @@ namespace InvoiceTotal
             this.txtDiscountAmount.Name = "txtDiscountAmount";
             this.txtDiscountAmount.ReadOnly = true;
             this.txtDiscountAmount.Size = new System.Drawing.Size(100, 20);
-            this.txtDiscountAmount.TabIndex = 11;
+            this.txtDiscountAmount.TabIndex = 7;
             this.txtDiscountAmount.TabStop = false;
             // 
             // txtTotal
@@ -158,7 +160,7 @@ namespace InvoiceTotal
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.ReadOnly = true;
             this.txtTotal.Size = new System.Drawing.Size(100, 20);
-            this.txtTotal.TabIndex = 12;
+            this.txtTotal.TabIndex = 9;
             this.txtTotal.TabStop = false;
             // 
             // frmInvoiceTotal
